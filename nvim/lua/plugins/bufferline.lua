b799a20bec610798b980ad93512096a6bea451a7
snchz-xtrm @extremeScandalist <sn_elh-92@mail.ru>
2024-05-21 16:24:45 +1000

    nvim: bufferline offset nvim tree

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,1 +1,12 @@
-require('bufferline').setup{}
+require('bufferline').setup{
+	options = {
+		offsets = {
+			{
+				filetype = "NvimTree",
+				text = "File Explorer",
+				highlight = "Directory",
+				separator = false -- use a "true" to enable the default, or set your own character
+			}
+		},
+	}
+}
