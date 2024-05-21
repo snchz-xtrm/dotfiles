2731db19f9f4e269928ef59efb63e6c64128672e
snchz-xtrm @extremeScandalist <sn_elh-92@mail.ru>
2024-05-21 16:43:03 +1000

    gitignore: remove nvim configuration on another repo

--- nvim/lua/base/search.lua
+++
@@ -1,8 +0,0 @@
--- Variables
-local opt = vim.opt -- vim options
-local g = vim.g     -- vim global variables
-
--- Search settings
-opt.ignorecase=true -- ignore case with search
-opt.smartcase=true  -- not ignore first letter capital case
-opt.showmatch=true  -- highlight founded objects
